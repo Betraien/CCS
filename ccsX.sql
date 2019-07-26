5747cba70e007d76841372c4c5dc4031060ded79
moh2099 <moh_2323@hotmail.com>
2019-07-26 08:32:03 +0300

    Update ccsX.sql

--- ccsX.sql
+++ ccsX.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2019 at 10:25 AM
+-- Generation Time: Jul 26, 2019 at 07:29 AM
 -- Server version: 10.3.16-MariaDB
--- PHP Version: 7.1.30
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -47,9 +47,9 @@ INSERT INTO `client_third_parties` (`id`, `client_id`, `platform_id`, `third_par
 (2, 6, 1, 5, '2019-07-04 00:00:00', '2019-07-11 00:00:00', 0),
 (3, 6, 1, 2, '2019-07-12 07:11:00', '2019-07-17 00:00:00', 0),
 (4, 3, 1, 3, '2019-07-05 00:00:00', '2019-07-12 00:00:00', 0),
-(5, 3, 2, 2, '2019-07-16 11:53:10', '2019-07-16 11:53:10', 1),
+(5, 3, 2, 2, '2019-07-16 11:53:10', '2019-07-16 11:53:10', 0),
 (6, 6, 3, 5, '2019-07-11 00:00:00', '2019-07-12 00:00:00', 0),
-(7, 6, 4, 1, '2019-07-21 16:10:04', '2019-07-21 16:10:04', 0),
+(7, 1, 2, 47, '2019-07-21 16:10:04', '2019-07-21 16:10:04', 0),
 (8, 1, 5, 5, '2019-07-21 16:10:24', '2019-07-21 16:10:24', 0),
 (9, 2, 1, 3, '2019-07-21 16:10:28', '2019-07-21 16:10:28', 0),
 (10, 3, 0, 5, '2019-07-21 16:10:31', '2019-07-21 16:10:31', 0);
@@ -111,12 +111,12 @@ CREATE TABLE `third_parties` (
 --
 
 INSERT INTO `third_parties` (`id`, `title`, `id_token`, `description`, `logo`, `third_party_type_id`, `view_order`, `third_party_status_id`, `position`, `website`, `contact_person`, `contact_phone`, `contact_email`, `average_rating`, `config`, `public`, `created_at`, `updated_at`, `deleted`) VALUES
-(1, 'xxxrgerthx', 'TNT', 'This is a plagiarism nyfgthnoijmi', 'wregwrgrg/wergoihrg.com', 1, 3, 1, 'xwef', 'Turnitin.com', 'jafri jafri', '+96595', 'ohrwiuhg@woehiug.com', 0, '{\"config\":{\r\n\"url\":\"https://classera.tii-sandbox.com/api/v1/submissions\",\r\n\r\n\"header\": {\r\n\"X-Turnitin-Integration-Name\": \"ccs\",\r\n\"X-Turnitin-Integration-Version\": \"v1beta\",\r\n\"Authorization\": \"Bearer 18853c5ef5804f88bc58d90a12a98577\",\r\n\"Content-Type\":\" application/json\"},\r\n\r\n\"body\":{\r\n\"owner\": \"classera\",\r\n\"title\": \"classera_submission\"}\r\n}\r\n}', 1, '2019-07-10 00:00:00', '2019-07-26 00:00:00', 0),
-(2, 'BrainPop', 'er345fy36', 'BrainPop Engages students through animated movies, learning games, interactive quizzes, primary source activities, concept mapping, and more.', 'images/SWE Program-Prerequisite.png', 1, 2, 1, 'Content Gateway', 'https://www.brainpop.com/ ', 'Saud Khaled', '+9665897635', 'Saud_Khaled@gmail.com', 0, '{\r\n    \"config\": {\r\n        \"Auth\": \"RESTapi\",\r\n        \"type\": \"post\",\r\n        \"url\": \"https://classera.tii-sandbox.com/api/v1/submissions\",\r\n        \"header\": {\r\n            \"X-Turnitin-Integration-Name\": \"ccs\",\r\n            \"X-Turnitin-Integration-Version\": \"v1beta\",\r\n            \"Authorization\": \"Bearer 18853c5ef5804f88bc58d90a12a98577\",\r\n            \"Content-Type\": \" application/json\"\r\n        },\r\n        \"body\": {\r\n            \"owner\": \"classera\",\r\n            \"title\": \"classera_submission\"\r\n        }\r\n    }\r\n}', 1, '2019-07-19 10:00:17', '2019-07-31 06:26:25', 0),
+(1, 'get the title', 'TNT', 'hello mr.shabib', 'wregwrgrg/wergoihrg.com', 1, 3, 1, 'xwef', 'Turnitin.com', 'jafri jafri', '+96595', 'ohrwiuhg@woehiug.com', 4.58, '{\"config\":{\r\n\"url\":\"https://classera.tii-sandbox.com/api/v1/submissions\",\r\n\r\n\"header\": {\r\n\"X-Turnitin-Integration-Name\": \"ccs\",\r\n\"X-Turnitin-Integration-Version\": \"v1beta\",\r\n\"Authorization\": \"Bearer 18853c5ef5804f88bc58d90a12a98577\",\r\n\"Content-Type\":\" application/json\"},\r\n\r\n\"body\":{\r\n\"owner\": \"classera\",\r\n\"title\": \"classera_submission\"}\r\n}\r\n}', 1, '2019-07-10 00:00:00', '2019-07-25 14:22:50', 0),
+(2, 'Dropboxx', 'DB', 'hello mr.shabibb', 'wegwg/tyjtj', 1, 2, 1, 'Jeffri', 'www.dropbox.com', 'rtht erg', '+96658965223', 'ohrwiuhg@woehiug.com', 2.85, '{\r\n    \"config\": {\r\n        \"Auth\": \"OAuth\",\r\n        \"type\": \"Get\",\r\n        \"url\": \"https://www.dropbox.com/oauth2/authorize?client_id=jssbqykrnanwmd8&response_type=code&redirect_uri=http://localhost/GitHub/CCS/public/ThirdParty/token\",\r\n        \"header\": {},\r\n        \"body\": {}\r\n    }\r\n}', 1, '2019-07-19 10:00:17', '2019-07-25 13:58:01', 0),
 (3, 'C8Science', 'g6ek95k5', 'C8Science combines computer & physical exercises to develop the cognitive skills necessary to learn in the classroom and improve math and reading achievement. Assigned by the Director of Student Personnel and Special Services for students who qualify', '', 1, 3, 1, 'Content Gateway', 'https://www.c8sciences.com/', 'Thamer Ahmed', '+96658965223', 'Thamer_Ahmed@gmail.com', 0, '[serialized (platforms, roles, redirect_url, token, etc.)]', 1, '2019-07-19 10:00:17', '2019-07-31 06:26:25', 0),
 (4, 'DD@DD', 'wegweg', 'wegweg', 'images/Thanks.jpg', 1, 1, 1, '1', '1', '1', '1', '2', 0, '{\r\n\"url\":\"https://classera.tii-sandbox.com/api/v1/submissions\",\r\n\r\n\"header\":[\r\n\"X-Turnitin-Integration-Name: ccs\",\r\n\"X-Turnitin-Integration-Version: v1beta\",\r\n\"Authorization: Bearer 18853c5ef5804f88bc58d90a12a98577\",\r\n\"Content-Type: application/json\"],\r\n\r\n\"body\":{\r\n\"owner\": \"classera\",\r\n\"title\": \"classera_submission\"}\r\n\r\n}', 1, '2019-07-07 18:14:59', '2019-07-07 18:14:59', 0),
-(5, 'Dropbox', 'DB', 'asd', 'wegwg/tyjtj', 1, 2, 1, 'Content Gateway', 'www.dropbox.com', 'rtht erg', '+96658965223', 'ohrwiuhg@woehiug.com', 2.85, '{\r\n    \"config\": {\r\n        \"Auth\": \"OAuth\",\r\n        \"type\": \"Get\",\r\n        \"url\": \"https://www.dropbox.com/oauth2/authorize?client_id=jssbqykrnanwmd8&response_type=code&redirect_uri=http://localhost/laravel/Blog/public/ThirdParty/token\",\r\n        \"header\": {},\r\n        \"body\": {}\r\n    }\r\n}', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0),
-(47, 'sad 1', 'noob', 'it is decri', 'logoPath', 1, 3, 1, 'xxxx', 'https://jeff.com', 'Jeff John', '+1 659 875 469', 'Jeff@jemmy.jeff', 0, '{\"config\":\"{\\\"config\\\":{\\\"url\\\":\\\"www.shabib.com\\\",\\\"header\\\":{\\\"Authorization\\\":\\\"0599193936\\\",\\\"content-type\\\":\\\"application\\\\\\/json\\\"}}}\"}', 1, '2019-07-21 08:38:53', '2019-07-21 08:38:53', 1),
+(5, 'Dropboxx', 'DB', 'hello mr.shabibb', 'wegwg/tyjtj', 1, 2, 1, 'Jeffri', 'www.dropbox.com', 'rtht erg', '+96658965223', 'ohrwiuhg@woehiug.com', 2.85, '{\"config\":\"{\\r\\n    \\\"config\\\": {\\r\\n        \\\"Auth\\\": \\\"OAuth\\\",\\r\\n        \\\"type\\\": \\\"Get\\\",\\r\\n        \\\"url\\\": \\\"https:\\/\\/www.dropbox.com\\/oauth2\\/authorize?client_id=jssbqykrnanwmd8&response_type=code&redirect_uri=http:\\/\\/localhost\\/GitHub\\/CCS\\/public\\/ThirdParty\\/token\\\",\\r\\n        \\\"header\\\": {},\\r\\n        \\\"body\\\": {}\\r\\n    }\\r\\n}\"}', 1, '0000-00-00 00:00:00', '2019-07-25 12:22:29', 0),
+(47, 'sad 1', 'noob', 'it is decri', 'logoPath', 1, 3, 1, 'xxxx', 'rth', 'Jeff John', '+1 659 875 469', 'Jeff@jemmy.jeff', 0, '{\"config\":\"{\\\"config\\\":{\\\"url\\\":\\\"www.shabib.com\\\",\\\"header\\\":{\\\"Authorization\\\":\\\"0599193936\\\",\\\"content-type\\\":\\\"application\\\\\\/json\\\"}}}\"}', 0, '2019-07-21 08:38:53', '2019-07-21 08:38:53', 0),
 (48, 'wefwegeg', 'sd', 'it is decri', 'logoPath', 1, 5, 1, 'wgweg', 'https://jeff.com', 'Jeff John', '+1 659 875 469', 'Jeff@jemmy.jeff', 0, '{\"config\":\"{\\\"config\\\":{\\\"url\\\":\\\"www.shabib.com\\\",\\\"header\\\":{\\\"Authorization\\\":\\\"0565826926\\\",\\\"content-type\\\":\\\"application\\\\\\/json\\\"}}}\"}', 1, '2019-07-21 10:05:59', '2019-07-21 10:05:59', 0),
 (49, 'Gtyhtyjtyjtyjtyjty 1', 'noob', 'it is decri', 'logoPath', 1, 5, 1, 'xxxx', 'https://jeff.com', 'Jeff John', '+1 659 875 469', 'Jeff@jemmy.jeff', 0, '{\"config\":{\"url\":\"www.shabib.com\",\"header\":{\"Authorization\":\"0599193936\",\"content-type\":\"application\\/json\"}}}', 1, '2019-07-21 13:28:56', '2019-07-21 13:28:56', 0),
 (50, 'wef 1', 'noob', 'it is decri', 'logoPath', 1, 5, 1, 'xxxx', 'https://jeff.com', 'Jeff John', '+1 659 875 469', 'Jeff@jemmy.jeff', 0, '{\"config\":{\"url\":\"www.shabib.com\",\"header\":{\"Authorization\":\"0599193936\",\"content-type\":\"application\\/json\"}}}', 1, '2019-07-21 13:29:07', '2019-07-21 13:29:07', 0),
@@ -251,20 +251,19 @@ CREATE TABLE `user_third_parties` (
 --
 
 INSERT INTO `user_third_parties` (`id`, `user_id`, `platform_id`, `client_id`, `third_party_id`, `token`, `status`, `expire_date`, `created_at`, `updated_at`, `deleted`) VALUES
-(1, 3, 2, 2, 5, '', 'Active', '2019-07-03 03:12:05', '2019-07-10 04:00:16', '2019-07-19 07:23:00', 0),
-(2, 3, 1, 2, 2, 'vendor123647', 'Active', '2019-07-03 03:12:05', '2019-03-03 10:00:16', '2019-09-19 12:23:00', 0),
-(3, 2, 1, 3, 5, 'vendor373783', 'xxxxx', '2019-07-03 03:12:05', '2019-03-03 10:00:16', '2019-09-19 12:23:00', 0),
-(4, 10, 8, 2, 19, '690d80c5-6a8b-41b5-80e1-baf42624a2fc', 'Active', '2019-07-14 09:28:33', '2019-07-14 09:28:33', '2019-07-14 09:28:33', 0),
-(5, 3, 8, 3, 4, '9b5335ce-fa27-4faa-b43d-14d9a97ef5e5', 'Active', '2019-07-14 09:30:22', '2019-07-14 09:30:22', '2019-07-14 09:30:22', 0),
-(24, 0, 0, 0, 2, 'e789b52d-963a-472d-9020-4017cc2bd90f', 'Active', '2019-07-21 07:49:30', '2019-07-21 07:49:30', '2019-07-21 07:49:30', 0),
-(25, 0, 1, 0, 5, 'YQR3vaGQARAAAAAAAAAAIEfT0kMkoJ4HYELWIyDkYIs', 'Active', '2019-07-21 08:22:46', '2019-07-21 08:22:46', '2019-07-21 08:22:46', 0),
-(26, 0, 1, 0, 4, 'YQR3vaGQARAAAAAAAAAAIfnRPF8M04v6J1PQ99533Ew', 'Active', '2019-07-21 08:23:25', '2019-07-21 08:23:25', '2019-07-21 08:23:25', 0),
-(27, 5, 5, 5, 2, 'c3170340-00aa-48cc-bfe5-1e0c76925ff7', 'Active', '2019-07-21 08:27:46', '2019-07-21 08:27:46', '2019-07-21 08:27:46', 0),
-(28, 2, 2, 3, 5, 'YQR3vaGQARAAAAAAAAAAIlLlubvI9oLK3ullmBUjv-Q', 'Active', '2019-07-21 09:26:14', '2019-07-21 09:26:14', '2019-07-21 09:26:14', 1),
-(29, 7, 2, 9, 2, '6f166696-5710-48d8-91bd-a8345d5aea25', 'Active', '2019-07-21 10:29:50', '2019-07-21 10:29:50', '2019-07-21 10:29:50', 0),
-(30, 5, 7, 6, 5, 'YQR3vaGQARAAAAAAAAAAIydg3lrkLlfbiP2sxRVM6Ow', 'Active', '2019-07-21 10:31:24', '2019-07-21 10:31:24', '2019-07-21 10:31:24', 0),
-(31, 45, 78, 65, 5, 'YQR3vaGQARAAAAAAAAAAJMHRgA_H3UzGL5of-7WVyS0', 'Active', '2019-07-21 10:32:19', '2019-07-21 10:32:19', '2019-07-21 10:32:19', 0),
-(32, 99, 99, 99, 2, '581815b3-f56f-4950-ae4f-5a02b90f7fc7', 'Active', '2019-07-21 10:32:54', '2019-07-21 10:32:54', '2019-07-21 10:32:54', 0);
+(1, 3, 2, 3, 5, '', 'Active', '2019-07-03 03:12:05', '2019-07-10 04:00:16', '2019-07-19 07:23:00', 0),
+(2, 3, 2, 3, 2, 'vendor123647', 'Active', '2019-07-03 03:12:05', '2019-03-03 10:00:16', '2019-09-19 12:23:00', 0),
+(3, 3, 2, 3, 3, 'vendor373783', 'xxxxx', '2019-07-03 03:12:05', '2019-03-03 10:00:16', '2019-09-19 12:23:00', 0),
+(39, 2, 5, 3, 5, 'YQR3vaGQARAAAAAAAAAAMMvJjbulCSKWugpTHe0sg1s', 'Active', '2019-07-25 08:15:11', '2019-07-25 08:15:11', '2019-07-25 08:15:11', 0),
+(43, 2, 8, 2, 5, 'YQR3vaGQARAAAAAAAAAAM0JcxgGuEKql8w5K3Epi1gQ', 'Active', '2019-07-25 08:52:50', '2019-07-25 08:52:50', '2019-07-25 08:52:50', 0),
+(45, 2, 9, 2, 5, 'YQR3vaGQARAAAAAAAAAAM0JcxgGuEKql8w5K3Epi1gQ', 'Active', '2019-07-25 08:54:35', '2019-07-25 08:54:35', '2019-07-25 08:54:35', 0),
+(46, 2, 6, 2, 5, 'YQR3vaGQARAAAAAAAAAAM0JcxgGuEKql8w5K3Epi1gQ', 'Active', '2019-07-25 09:00:36', '2019-07-25 09:00:36', '2019-07-25 09:00:36', 0),
+(47, 23, 23, 412, 5, 'YQR3vaGQARAAAAAAAAAANNDLSB7mW2BuGPwxHS3Eno4', 'Active', '2019-07-25 09:19:43', '2019-07-25 09:19:43', '2019-07-25 09:19:43', 0),
+(48, 2, 16, 4, 5, 'YQR3vaGQARAAAAAAAAAANVhrcZaWKrMiABIKlson50s', 'Active', '2019-07-25 09:21:49', '2019-07-25 09:21:49', '2019-07-25 09:21:49', 0),
+(49, 232, 232, 232, 5, 'YQR3vaGQARAAAAAAAAAANr-wBJaqdtfu0dX5SkZQyP0', 'Active', '2019-07-25 09:26:58', '2019-07-25 09:26:58', '2019-07-25 09:26:58', 0),
+(51, 232, 123, 123, 5, 'YQR3vaGQARAAAAAAAAAAOO8oRm_LMVVC0XvkvW1BXtE', 'Active', '2019-07-25 09:53:22', '2019-07-25 09:53:22', '2019-07-25 09:53:22', 0),
+(52, 7, 7, 9, 5, 'YQR3vaGQARAAAAAAAAAAOXyVp3KdpPMasn70dBZX7vo', 'Active', '2019-07-25 10:06:00', '2019-07-25 10:06:00', '2019-07-25 10:06:00', 0),
+(56, 1, 4, 1, 5, 'YQR3vaGQARAAAAAAAAAAPTxkqkoLwk6UDo2wLkI0Qb0', 'Active', '2019-07-25 10:11:49', '2019-07-25 10:11:49', '2019-07-25 10:11:49', 0);
 
 --
 -- Indexes for dumped tables
@@ -395,7 +394,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `user_third_parties`
 --
 ALTER TABLE `user_third_parties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
